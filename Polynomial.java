dc4bfe563aa32db07435301286bcd7bb24c714aa
Arihant Kumar Seraogi <arihantseraogi@gmail.com>
2017-04-06 23:53:28 +0530

    Final Version use this

--- Polynomial.java
+++ Polynomial.java
@@ -1,55 +1,51 @@
+import java.util.Scanner;
+
 public class Polynomial 
 {
- private int[] coef;  // coefficients
+ private int coef[];  // coefficients
  private int deg;     // degree of polynomial (0 for the zero polynomial)
-
-
- public Polynomial(int a, int b) // a * x^b
+ Scanner s=new Scanner(System.in);
+ private void getDegree()
  {
-        coef = new int[b+1]; //increase in size of array after each term is added
-        coef[b] = a;         //setting the coefficient for respective exponent value here represented by index value
-        deg = degree();      //fetching the new degree after the addition of the abpve term
+	 
+	 System.out.println("Enter the highest degree for the Polynomial");
+	 deg=s.nextInt();
+ } 
+ private void getCoef()
+ {
+	 for(int i =coef.length-1; i >= 0; i--)
+	 {
+		 System.out.println("Enter the Coefficient value when exponent of the term is\t"+i);
+		 coef[i]=s.nextInt();
+	 }
+	 s.close();
  }
 
-    
- public int degree() // return the degree of this polynomial (0 for the zero polynomial)
+ public Polynomial()
  {
-        int d = 0;                            
-        for (int i = 0; i < coef.length; i++) // traversing the entire array length
-            if (coef[i] != 0) d = i;          // calculating the highest degree for which a valid coefficient is form
-        return d;                             //highest degree is returned
- }
- public Polynomial times(Polynomial b)
- {
-         Polynomial a = this;                                // a is a Polynomial class object which is initialised here by the object instance by which this method was invoked
-         Polynomial c = new Polynomial(0, a.deg + b.deg);
-         for (int i = 0; i <= a.deg; i++)
-             for (int j = 0; j <= b.deg; j++)
-                 c.coef[i+j] += (a.coef[i] * b.coef[j]);
-         c.deg = c.degree();
-         return c;
- }
- 
- public Polynomial plus(Polynomial b) // return c = a + b
- {
-     Polynomial a = this;
-     Polynomial c = new Polynomial(0, Math.max(a.deg, b.deg));
-     for (int i = 0; i <= a.deg; i++) c.coef[i] += a.coef[i];
-     for (int i = 0; i <= b.deg; i++) c.coef[i] += b.coef[i];
-     c.deg = c.degree();
-     return c;
+        getDegree();      
+        coef=new int[deg+1];
+        getCoef();
+        
  }
 
-    
- public Polynomial eval(Polynomial b) // return a(b(x))  - compute using Horner's method
+ public double evaluate(int x)
  {
-        Polynomial a = this;
-        Polynomial c = new Polynomial(0, 0);
-        for (int i = a.deg; i >= 0; i--)
-        {
-            Polynomial term = new Polynomial(a.coef[i], 0);
-            c = term.plus(b.times(c));
-        }
-        return c;
+	 double result = 0;
+     for (int i = coef.length-1; i >= 0; i--)
+         result = coef[i] + (x * result);
+     return result;
  }
+
+
+
+	/*public static void main(String[] args) //for testing
+	{
+		Polynomial p1;
+		p1=new Polynomial();
+		double c=p1.evaluate(2);
+		System.out.println(c);
+
+	}*/
+
 }
