a9345d253b2684df976b8b09c9adca83a4f0ed4c
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2017-04-18 18:19:34 +0530

    Update

--- Polynomial.java
+++ Polynomial.java
@@ -2,42 +2,40 @@ import java.util.Scanner;
 
 public class Polynomial 
 {
- private int coef[];  // coefficients
- private int deg;     // degree of polynomial (0 for the zero polynomial)
- Scanner s=new Scanner(System.in);
- private void getDegree()
- {
-	 
-	 System.out.println("Enter the highest degree for the Polynomial");
-	 deg=s.nextInt();
- } 
- private void getCoef()
- {
-	 for(int i =coef.length-1; i >= 0; i--)
-	 {
-		 System.out.println("Enter the Coefficient value when exponent of the term is\t"+i);
-		 coef[i]=s.nextInt();
-	 }
-	 s.close();
- }
+	private int coef[];  // coefficients
+	private int deg;     // degree of polynomial (0 for the zero polynomial)
+	Scanner s = new Scanner(System.in);
+	
+	private void getDegree()
+	{
+		System.out.println("Enter the highest degree for the Polynomial");
+		deg = s.nextInt();
+	} 
+	
+	private void getCoef()
+	{
+		for(int i = coef.length - 1; i >= 0; i--)
+		{
+			System.out.println("Enter the Coefficient value when exponent of the term is\t"+i);
+			coef[i] = s.nextInt();
+		}
+		s.close();
+	}
 
- public Polynomial()
- {
-        getDegree();      
-        coef=new int[deg+1];
-        getCoef();
-        
- }
+	public Polynomial()
+	{
+		getDegree();      
+        coef = new int[deg + 1];
+        getCoef();       
+	}
 
- public double evaluate(int x)
- {
-	 double result = 0;
-     for (int i = coef.length-1; i >= 0; i--)
-         result = coef[i] + (x * result);
-     return result;
- }
-
-
+	public double evaluate(double x)
+	{
+		double result = 0;
+		for (int i = coef.length-1; i >= 0; i--)
+			result = coef[i] + (x * result);
+		return result;
+	}
 
 	/*public static void main(String[] args) //for testing
 	{
@@ -47,5 +45,4 @@ public class Polynomial
 		System.out.println(c);
 
 	}*/
-
 }
