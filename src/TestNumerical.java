ce4d79601e17a7b4493b87451b0f90a6cdd2173d
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2017-04-22 14:32:23 +0530

    Test Case Update

--- src/TestNumerical.java
+++ src/TestNumerical.java
@@ -7,14 +7,23 @@ public class TestNumerical {
 		Scanner in = new Scanner(System.in); // this is just to test the successful creation of a Scanner class object
 		System.out.println("Please enter your name.");
 		String name = in.nextLine();
-		System.out.println(name);
+		//in.nextLine();
+		System.out.println("Name is " + name);
 		System.out.println("Regula Falsi Method");
-		RegulaFalsiMethod rf = new RegulaFalsiMethod();
-		rf.calcFalseRoot();
+		//RegulaFalsiMethod rf = new RegulaFalsiMethod();
+		//rf.calcFalseRoot();
 		System.out.println("Bisection Method");
 		BisectionMethod obj = new BisectionMethod();
 		obj.calcBisectionRoot();
+		System.out.println("Backward Interpolation");
+		Backward back = new Backward();
+		back.backwardCal();
+		System.out.println("Trapezoidal Rule");
+		Trapezoidal trap = new Trapezoidal();
+		trap.integrateTrapezoidal();
+		System.out.println("Simpsons Rule");
+		Simpson sim = new Simpson();
+		sim.integrateSimpson();
 		in.close();
 		}
-
 }
